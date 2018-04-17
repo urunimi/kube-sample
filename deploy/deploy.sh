ea572965e8f163dca4a356569c6fe6c9a41e76d3
Ben Pyungwoo Yoo <hovan@hovans.com>
2018-04-18 00:03:36 +0900

    deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 set -e
 
-git checkout deploy/kube/
+git checkout kube/
 
 export GIT_REVISION=`git rev-parse --short HEAD`
 export TAG="$GIT_REVISION"
 bash ./deploy/build-docker.sh
-sed -i.bak "s/latest/${GIT_REVISION}/" deploy/kube/app-hello-go.yaml
+sed -i.bak "s/latest/${GIT_REVISION}/" kube/app-hello-go.yaml
 kubectl config use-context k8s-test.honeyscreen.com
-kubectl replace -f deploy/kube/app-hello-go.yaml
-rm deploy/kube/app-hello-go.yaml.bak
-git checkout deploy/kube/
+kubectl replace -f kube/app-hello-go.yaml
+rm kube/app-hello-go.yaml.bak
+git checkout kube/
 kubectl rollout status deploy hello-go
